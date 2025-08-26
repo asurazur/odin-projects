a74c6cb81cb0a10497294d990dae3f58773f47a5
asurazur <asurazur@gmail.com>
2025-08-26 13:39:01 +0800

    feat: add base functionality

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('#container')
+const CONTAINERSIZE = '960px'
+container.style.width = CONTAINERSIZE
+container.style.height = CONTAINERSIZE
+
+function setDimension(dimension) {
+    container.replaceChildren()
+    for (let i = 0; i < Math.pow(dimension, 2); i++) {
+        const square = document.createElement('div')
+        const gridSize = parseInt(CONTAINERSIZE)/dimension
+        square.classList.add('square')
+        square.style.width = square.style.height = gridSize + 'px'
+        container.appendChild(square)
+    }
+    
+    const squares = document.querySelectorAll('.square')
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.toggle('hovered')
+        })
+    })
+}
+
+const dimensionBtn = document.querySelector('#dimension-btn')
+dimensionBtn.addEventListener('click', () => {
+    let newDim = prompt('Enter new grid dimension (max 100): ')
+    if(newDim){
+        const gridDim = Math.trunc(parseInt(newDim))
+        setDimension(gridDim)
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    setDimension(16)
+});
