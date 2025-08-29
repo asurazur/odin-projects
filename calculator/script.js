ec8cbd55b6cc915473a2ff33edbfaa6d8ed23410
asurazur <asurazur@gmail.com>
2025-08-29 14:36:49 +0800

    feat: add calculator logic

--- calculator/script.js
+++ calculator/script.js
@@ -1,4 +1,6 @@
-let num1, num2, operator;
+let accumulator = '';
+let operand = '';
+let operator = '';
 
 // Add Event Listener to each button
 document.addEventListener('click', (e) => {
@@ -8,11 +10,69 @@ document.addEventListener('click', (e) => {
 
     // Get The Properties of the Button
     let [id, classList] = getTargetProperties(e.target);
+    
+    // If the button is an operand 
+    if(classList.contains('btn-operand')) {
+        // Update `operand` variable
+        if (id === 'decimal' && operand.includes('.')) {
+            return
+        }
+        operand += (e.target.textContent)
+    }
 
-    // If the button is an operand append to the display
-    // If the button is a operator, evaluate the equation
+    // If the button is a operator, update `operator button`
+    else if(classList.contains('btn-operator') || classList.contains('btn-other')) {
+        if (id === 'clear') {
+            accumulator = ''
+            operand = ''
+            operator = ''
+        }
+        else if (id === 'plus-minus') {
+            operand = -operand
+        }
+        else if (id === 'eq') {
+            if (accumulator === '' || operator === '' || operand === '') { 
+                return
+            }
+            accumulator = operate(Number(accumulator), operator, Number(operand))
+            operand = ''
+        }
+        else {
+            operator = id
+            if (accumulator === '') {
+                accumulator = Number(operand)
+                operand = ''
+            }
+        }
+    }
+    // Update Display
+    updateDisplay()
 })
 
 function getTargetProperties(target) {
-    return [e.target.id, e.target.classList]
+    return [target.id, target.classList]
+}
+
+
+function updateDisplay() {
+    document.getElementById('display').textContent = Number(operand) || Number(accumulator) || '0'
+}
+
+function operate(accumulator, operator, operand) {
+    if (operator === 'add') {
+        return accumulator + operand
+    }
+    else if (operator === 'minus') {
+        return accumulator - operand
+    }
+    else if (operator === 'multiply') {
+        return accumulator * operand
+    }
+    else if (operator ==='divide') {
+        return accumulator / operand
+    }
+    else if (operator === 'modulo') {
+        return accumulator % operand
+    }
+
 }
