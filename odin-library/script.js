017f7fdbb90b1d53ee8faad9f2087555ce32b08c
asurazur <asurazur@gmail.com>
2025-09-11 11:06:04 +0800

    feat: add dummy data

--- odin-library/script.js
+++ odin-library/script.js
@@ -14,6 +14,12 @@ function addBookToLibrary(title, author, pages, read, date) {
     const book = new Book(id, title, author, pages, read, date);
     myLibrary.push(book);
 }
+addBookToLibrary("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, true, "June 26, 1997");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false, "September 21, 1937");
+addBookToLibrary("1984", "George Orwell", 328, true, "June 8, 1949");
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false, "July 11, 1960");
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true, "April 10, 1925");
+
 // Loop through the array and display each book on the page
 
 // Add a “New Book” button that brings up a form allowing users to input the details for the new book and add it to the library: author, title, number of pages, whether it’s been read and anything else you might want.
