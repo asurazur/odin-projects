e1f50f1517550f0a124a380a133b5900f0bf12d8
asurazur <asurazur@gmail.com>
2025-09-24 08:36:31 +0800

    feat: add id to buttons

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+import "./modules/home.js";
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code that interacts with the DOM goes here
+    const homeButton = document.querySelector("#home-btn");
+    const menuButton = document.querySelector("#menu-btn");
+    const aboutButton = document.querySelector("#about-btn");
+    const contactButton = document.querySelector("#contact-btn");
+
+    homeButton.addEventListener('click', () => {
+        homeButton.focus();
+        loadHome();
+    })
+});
