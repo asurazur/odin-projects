6fc800b0ee1bdfb373d260aae896ae8eb07d854e
asurazur <asurazur@gmail.com>
2025-09-24 12:08:48 +0800

    feat: load  testimonial function  added to javascript

--- restaurant-page/src/modules/home.js
+++ restaurant-page/src/modules/home.js
@@ -85,14 +85,20 @@ export function loadTestimonials(){
         {
             description: 'Rakuzen has quickly become our new go-to spot for satisfying ramen cravings. The service is incredibly warm and accommodating, making each visit feel genuinely welcoming.',
             stars: 5,
+            image: "M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z",
+            name: 'User',
         },
         {
             description: 'So far we\'ve dined here 3x and food and service has been very consistent. they now also have free fruity-minty candies that they give out to customers when you bill out.',
             stars: 5,
+            image: "M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z",
+            name: 'User',
         },
         {
             description: '3 ramen bowls, Gyoza, Nori fries.  Delicious and worth it! Staff were super nice, fast service, and no judgment even in slippers, Open 5–9PM only, so plan ahead!',
             stars: 5,
+            image: "M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z",
+            name: 'User',
         },
     ]
 
@@ -105,12 +111,12 @@ export function loadTestimonials(){
         const quote = document.createElement('q');
         quote.classList.add('p1');
         quote.innerText = testimony.description;
-        container.appendChild(quote);
+        card.appendChild(quote);
 
         // Stars
+        const svgns = "http://www.w3.org/2000/svg";
         const stars = document.createElement('div');
         for(let i=0; i<testimony.stars; ++i){
-            const svgns = "http://www.w3.org/2000/svg";
             // Create the <svg> element
             const svg = document.createElementNS(svgns, "svg");
             svg.setAttribute("xmlns", svgns);
@@ -127,14 +133,41 @@ export function loadTestimonials(){
             svg.appendChild(path);
             stars.appendChild(svg);
         }
-        container.appendChild(stars);
-
+        card.appendChild(stars);
+        
         // User Image
+        const userDiv = document.createElement('div');
+            // Create Profile
+        const userSvg = document.createElementNS('http://www.w3.org/2000/svg', "svg");
+        userSvg.setAttribute("height", "24px");
+        userSvg.setAttribute("width", "24px");
+        userSvg.setAttribute("viewBox", "0 -960 960 960");
+        userSvg.setAttribute("fill", "#000000");
+        const userPath = document.createElementNS(svgns, "path");
+        userPath.setAttribute("d", testimony.image);
+        userSvg.appendChild(userPath);
+        userDiv.appendChild(userSvg);
+        
+
+        const userName = document.createElement('p');
+        userName.classList.add('p2'); 
+        userName.innerText = testimony.name;
+        userDiv.appendChild(userName);
+        
+        card.appendChild(userDiv)
 
         // HyperLink
+        const linkDiv = document.createElement('div');
+        const hyperlink = document.createElement('a');
+        hyperlink.classList.add('hyperlink');
+        hyperlink.innerText = 'Review';
+        linkDiv.appendChild(hyperlink);
+        card.appendChild(linkDiv);
+
+        container.appendChild(card);
     })
     section.appendChild(container);
-
+    content.appendChild(section);
 }
 
 // <div id="testimonials">
