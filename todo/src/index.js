09e59d4c361106a0a06b094f7313283164a7b3a3
asurazur <asurazur@gmail.com>
2025-09-29 11:56:55 +0800

    fix: remove debugging code

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,12 @@
-import { Project } from './js/project.js';
-import { ToDo } from './js/todo.js';
+import { ProjectController } from './controllers/ProjectController';
+import './styles/styles.css';
 
-const project = new Project("Default")
-project.todo = new ToDo('Clean Kitchen', 'Wash the dishes', new Date("09/28/2025"), 'medium')
-project.todo = new ToDo('Buy Groceries', 'Go to Robinsons', new Date("09/28/2025"), 'medium')
-console.log(project.todo)
+// const projectList = [];
+
+// let project = new Project("Home");
+// project.todo = new ToDo('Clean Kitchen', 'Wash the dishes', new Date("09/28/2025"), 'medium')
+// project.todo = new ToDo('Buy Groceries', 'Go to Robinsons', new Date("09/28/2025"), 'medium')
+// projectList.push(project);
+// console.log(projectList)
+
+new ProjectController();
