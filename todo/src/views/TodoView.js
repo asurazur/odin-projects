3b459cd315c480875b2704cc147e7a305a60b477
asurazur <asurazur@gmail.com>
2025-09-29 13:16:57 +0800

    feat: add Modal Initialization

--- todo/src/views/TodoView.js
+++ todo/src/views/TodoView.js
@@ -1,5 +1,41 @@
 export class TodoView {
+    // Handles The Todo of an Active PRoject
     constructor(){
-        // Initialize UI Components
+        // Instansiate UI components of Todo
+        this.addButton = document.querySelector('#add-todo');
+        this.modal = document.querySelector('#todo-dialog');
+        this.cancelBtn = this.modal.querySelector('#cancel-btn')
+        this.form = document.querySelector('#todo-form');
+        this.#setUpModal();
+    }
+
+    bindAddTodo(handler){
+        this.form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            handler(new FormData(this.form));
+            this.form.reset();
+            this.modal.close();
+        })
+    }
+
+    #setUpModal() {
+        this.addButton.addEventListener('click', (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            // Show Modal
+            this.modal.showModal();
+        })
+        this.cancelBtn.addEventListener('click', (e) => {
+            this.modal.close();
+        })
+
+    }
+
+    bindRemoveTodo(handler){
+        // 
+    }
+    
+    displayTodos(todos) {
+        //
     }
 }
