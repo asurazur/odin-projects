26dc3abb7d7802ac12cb577ac325e9a391ccd357
asurazur <asurazur@gmail.com>
2025-10-06 18:16:23 +0800

    feat: create append functionality

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -11,4 +11,20 @@ export class LinkedList {
     this.tail = null;
     this.size = 0;
   }
+  /**
+   * Adds a new node containing value to the end of the list
+   * @param {string} value - The data of the node
+   */
+  append(value) {
+    const newNode = new Node(value);
+    // If head is null, Make the newNode head
+    if (this.size === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
 }
