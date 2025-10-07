a9d1bc0e2d2cce0b840763765a1453156979280e
asurazur <asurazur@gmail.com>
2025-10-07 11:08:19 +0800

    feat: export the HashMap Class

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,6 +1,6 @@
 import { LinkedList } from "./linked-list.js";
 
-class HashMap {
+export class HashMap {
   constructor(capacity = 16, loadFactor = 0.75) {
     this.capacity = capacity;
     this.loadFactor = loadFactor;
