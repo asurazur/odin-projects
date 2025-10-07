1d3545216fc63a09b8fd45657c02db2bac4a76ce
asurazur <asurazur@gmail.com>
2025-10-07 14:24:28 +0800

    feat: add prettyPrint function

--- binary-search-tree/binary-search-tree.js
+++ binary-search-tree/binary-search-tree.js
@@ -23,4 +23,21 @@ export class Tree {
     rootNode.right = this.#_buildTree(array.toSpliced(0, mid + 1));
     return rootNode;
   }
+
+  prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
 }
