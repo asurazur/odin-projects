2b17de22e866e8277974f6ee91bd6d7d1259b608
asurazur <asurazur@gmail.com>
2025-10-08 11:06:34 +0800

    feat: remove position class and create helper functions

--- knights-travails/main.js
+++ knights-travails/main.js
@@ -1,33 +1,38 @@
-class Node {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  getDelta() {
-    return [
-      [-2, -1],
-      [-2, +1],
-      [+2, -1],
-      [+2, +1],
-      [-1, -2],
-      [-1, +2],
-      [+1, -2],
-      [+1, +2],
-    ];
-  }
-
-  validMoves() {
-    let moves = this.getDelta();
-    moves.map((value) => {
-      value[0] = value[0] + this.x;
-      value[1] = value[1] + this.y;
-    });
-    moves = moves.filter((value) => {
-      return value[0] < 7 && value[0] >= 0 && value[1] < 7 && value[1] >= 0;
-    });
-    return moves;
-  }
+import { Queue } from "./queue.js";
+function getDelta() {
+  return [
+    [-2, -1],
+    [-2, +1],
+    [+2, -1],
+    [+2, +1],
+    [-1, -2],
+    [-1, +2],
+    [+1, -2],
+    [+1, +2],
+  ];
 }
 
-console.log(new Node(0, 0).validMoves());
+function validMoves(position) {
+  let moves = getDelta();
+  moves.map((value) => {
+    value[0] = value[0] + position[0];
+    value[1] = value[1] + position[1];
+  });
+  moves = moves.filter((value) => {
+    return value[0] <= 7 && value[0] >= 0 && value[1] <= 7 && value[1] >= 0;
+  });
+  return moves;
+}
+
+function isMatch(position, destination) {
+  return position.every((val, i) => val === destination[i]);
+}
+
+function isVisited(target, visited) {
+  return visited.some((row) => isMatch(row, target));
+}
+
+let current = [0, 0];
+let destination = [7, 7];
+let path = knightMoves(current, destination);
+console.log(path);
