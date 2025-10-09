5e6ecbeb7b56028228ee098e257d6f406d7c21c5
asurazur <asurazur@gmail.com>
2025-10-09 18:02:08 +0800

    feat(battleship): create Ship Class

---
+++ battleship/src/js/Ship.js
@@ -0,0 +1,23 @@
+/**
+ * Represents a ship in the game
+ */
+class Ship {
+  constructor(length) {
+    if (length < 1 || !Number.isInteger(length))
+      throw new Error("Ship's Length Must be Positive Integer");
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    if (this.length > this.hits) {
+      this.hits++;
+    }
+  }
+
+  isSunk() {
+    return this.hits === this.length;
+  }
+}
+
+export { Ship };
