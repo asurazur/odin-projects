f24e01bc6dbb790992fc35ef84b67b8414496bf9
asurazur <asurazur@gmail.com>
2025-10-09 14:19:13 +0800

    fix(testing-practice): restore stashed edit

--- testing-practice/main.js
+++ testing-practice/main.js
@@ -2,9 +2,24 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function reverseString(str) {}
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
-const calculator = {};
+const calculator = {
+  add: function add(x, y) {
+    return x + y;
+  },
+  subtract: function subtract(x, y) {
+    return x - y;
+  },
+  multiply: function multiply(x, y) {
+    return x * y;
+  },
+  divide: function divide(x, y) {
+    return x / y;
+  },
+};
 
 function caesarCipher(str, shift) {}
 
