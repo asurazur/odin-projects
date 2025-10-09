998172aec2aefe996f143f9dee388b77ae8b99bc
asurazur <asurazur@gmail.com>
2025-10-09 11:52:45 +0800

    feat(testing-practice): create function declaration

---
+++ testing-practice/main.js
@@ -0,0 +1,11 @@
+function capitalize(str) {}
+
+function reverseString(str) {}
+
+const calculator = {};
+
+function caesarCipher(str, shift) {}
+
+function analyzeArray(array) {}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
