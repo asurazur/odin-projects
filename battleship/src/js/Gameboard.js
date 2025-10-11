a532ceda9ddc257435a305f172491f3e673bb55d
asurazur <asurazur@gmail.com>
2025-10-11 13:10:44 +0800

    feat(battleship): create method for placing a ship on the board

--- battleship/src/js/Gameboard.js
+++ battleship/src/js/Gameboard.js
@@ -1,4 +1,5 @@
 import { Coordinate } from "./Coordinate.js";
+import { Ship } from "./Ship.js";
 
 class Gameboard {
   static BOARD_SIZE = 10;
@@ -9,55 +10,108 @@ class Gameboard {
   }
 
   initializeBoard() {
-    // 10 x 10 Boolean Array, to determine if the element is hit or not
+    // 10 x 10 Boolean Array: Ship | null
     this.board = [
       ...new Array(Gameboard.BOARD_SIZE)
         .fill(null)
-        .map(() => new Array(Gameboard.BOARD_SIZE).fill("")),
+        .map(() => new Array(Gameboard.BOARD_SIZE).fill(null)),
     ];
-    // const ships = {carrier: [5pairs], battleship: [4pairs], cruiser: [3pairs], sub: [3pairs], destroyer: [2pairs]}
-    this.ships = new Map([
-      ["carrier"],
-      ["battleship"],
-      ["cruiser"],
-      ["submarine"],
-      ["destroyer"],
-    ]);
+    this.misses = new Set();
+    this.hits = new Set();
+    this.ships = [];
   }
 
   #isOutofBounds(move) {
-    return move.getX() > Gameboard.MAX_COORDINATE ||
-      move.getY() > Gameboard.MAX_COORDINATE
+    const [x, y] = move.getCoordinate();
+    return x > Gameboard.MAX_COORDINATE ||
+      y > Gameboard.MAX_COORDINATE ||
+      x < 0 ||
+      y < 0
       ? true
       : false;
   }
 
-  // recieveAttack(x, y): {status: boolean, note: string} -> Determine if a ship is hit or not | already attacked
+  #checkOverlap(ship, coordinate, orientation) {
+    let [row, column] = coordinate.getCoordinate();
+    for (let i = 0; i < ship.length; ++i) {
+      if (orientation === "horizontal") {
+        if (this.board[row][column + i] !== null) {
+          return true;
+        }
+      } else if (orientation === "vertical") {
+        if (this.board[row + i][column] !== null) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  #populateCoordinates(ship, coordinate, orientation) {
+    let [row, column] = coordinate.getCoordinate();
+    for (let i = 0; i < ship.length; ++i) {
+      if (orientation === "horizontal") {
+        this.board[row][column + i] = ship;
+      } else if (orientation === "vertical") {
+        this.board[row + i][column] = ship;
+      }
+    }
+    return false;
+  }
+
+  // recieveAttack(Coordinate): {status: boolean, note: string} -> Determine if a ship is hit or not | already attacked
   recieveAttack(attack) {
     if (this.#isOutofBounds(attack)) {
       return { status: false, note: "outOfBounds" };
     }
 
-    const [x, y] = attack.getCoordinate();
-    const isShot = this.board[x][y] === "shot";
-    if (isShot) {
-      return { status: false, note: "shot" };
+    const stringCoordinate = attack.toString();
+    if (this.hits.has(stringCoordinate) || this.misses.has(stringCoordinate)) {
+      return { status: false, note: "alreadyAttacked" };
     }
 
-    if (this.ships.has(this.board[x][y])) {
-      const shipType = this.board[x][y];
-      this.ships.get(shipType).hit();
-      this.board[x][y] = "shot";
+    if (this.board[x][y] === null) {
+      this.misses.add(stringCoordinate);
+      return { status: true, note: "miss" };
+    } else {
+      this.board[x][y].hit();
+      this.hits.add(stringCoordinate);
       return { status: true, note: "ship" };
     }
-
-    this.board[x][y] = "shot";
-    return { status: true, note: "miss" };
   }
 
   placeShip(ship, coordinate, orientation) {
     // Able to Place Ships at Specific Coordinates, No Overlapping or Out of Bounds
-    // Returns True if successful, False if unsuccessful
+    if (orientation !== "horizontal" || orientation !== "vertical")
+      return { status: false, note: "invalidOrientation" };
+
+    const [row, column] = coordinate.getCoordinate();
+    if (this.#isOutofBounds(coordinate)) {
+      return { status: false, note: "outOfBounds" };
+    }
+
+    if (
+      orientation === "horizontal" &&
+      column + ship.length > Gameboard.BOARD_SIZE
+    ) {
+      return { status: false, note: "outOfBounds" };
+    }
+
+    if (
+      orientation === "vertical" &&
+      row + ship.length > Gameboard.BOARD_SIZE
+    ) {
+      return { status: false, note: "outOfBounds" };
+    }
+
+    // Check if the coordinate Overlaps
+    if (this.#checkOverlap(ship, coordinate, orientation))
+      return { status: false, note: "overlap" };
+
+    this.ships.push(ship);
+
+    this.#populateCoordinates(ship, coordinate, orientation);
+    return { status: true, note: "shipPlaced" };
   }
 
   allShipSunk() {
