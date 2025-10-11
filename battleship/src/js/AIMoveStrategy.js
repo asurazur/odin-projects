f51d1cadff0686f4b06674b478027b50bfc12da2
asurazur <asurazur@gmail.com>
2025-10-11 15:26:15 +0800

    feat(battleship): implement a base AI strategy

--- battleship/src/js/AIMoveStrategy.js
+++ battleship/src/js/AIMoveStrategy.js
@@ -1,5 +1,14 @@
 import { IMoveStrategy } from "./IMoveStrategy.js";
 
 export class AIMoveStrategy extends IMoveStrategy {
-  getMove(board) {}
+  getMove(board) {
+    let move;
+    do {
+      const x = Math.floor(Math.random() * boardSize);
+      const y = Math.floor(Math.random() * boardSize);
+      move = `[${x},${y}]`;
+    } while (board.misses.has(move) || board.hits.has(move));
+    const [x, y] = move.split(",").map(Number);
+    return { x, y };
+  }
 }
