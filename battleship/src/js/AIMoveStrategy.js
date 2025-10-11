8219425bc8e2822deebc761fc31bc2082246f4b3
asurazur <asurazur@gmail.com>
2025-10-11 16:17:07 +0800

    fix: remove the brackets of a move in AIMoveStrategy

--- battleship/src/js/AIMoveStrategy.js
+++ battleship/src/js/AIMoveStrategy.js
@@ -4,9 +4,9 @@ export class AIMoveStrategy extends IMoveStrategy {
   getMove(board) {
     let move;
     do {
-      const x = Math.floor(Math.random() * boardSize);
-      const y = Math.floor(Math.random() * boardSize);
-      move = `[${x},${y}]`;
+      const x = Math.floor(Math.random() * board.board.length);
+      const y = Math.floor(Math.random() * board.board.length);
+      move = `${x},${y}`;
     } while (board.misses.has(move) || board.hits.has(move));
     const [x, y] = move.split(",").map(Number);
     return { x, y };
