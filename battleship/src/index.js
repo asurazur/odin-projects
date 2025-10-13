b3a28dd8f5227d45b657727adb3ee37c01653b86
asurazur <asurazur@gmail.com>
2025-10-13 14:39:10 +0800

    feat(battlship): implemented game loop

--- battleship/src/index.js
+++ battleship/src/index.js
@@ -10,16 +10,16 @@ import { GameController } from "./js/GameController.js";
 const view = new View();
 
 // Two random AIs (for demonstration)
-const p1 = PlayerFactory.createPlayer("human", "One", new Gameboard());
+const p1 = PlayerFactory.createPlayer("human", "Player", new Gameboard());
 p1.moveStrategy.getMoveFn = async (opponentBoard) => {
   return new Promise((resolve) => {
     // View listens for cell click, then resolves the move
     view.onCellClick = (coord) => resolve(coord);
   });
 };
-p1.board.placeShip(new Ship(5), new Coordinate(0, 0), "horizontal");
-const p2 = PlayerFactory.createPlayer("ai", "Two", new Gameboard());
-p2.board.placeShip(new Ship(5), new Coordinate(0, 0), "vertical");
+p1.board.randomPlaceShip();
+const p2 = PlayerFactory.createPlayer("ai", "Computer", new Gameboard());
+p2.board.randomPlaceShip();
 
 const game = new Game(p1, p2);
 const controller = new GameController(game, view);
--- battleship/src/js/GameController.js
+++ battleship/src/js/GameController.js
@@ -13,9 +13,27 @@ class GameController {
     while (!isOver) {
       // Await move (works for both AI and Human)
       const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-      isOver = await this.game.performTurn();
+
+      // Show Who's Turn
+      await sleep(250);
+      this.view.updateMessage(`${this.game.currentPlayer.name}'s Turn`);
+      await sleep(250);
+
+      // Attempt Move
+      let log = { status: false };
+      while (!log.status) {
+        log = await this.game.performTurn();
+        this.view.updateMessage(log.note);
+      }
       this.view.renderBoards(this.game.player1.board, this.game.player2.board);
-      await sleep(1000);
+
+      if (this.game.isOver()) {
+        isOver = true;
+        this.view.updateMessage(`${this.game.getWinner().name} Wins`);
+        break;
+      }
+
+      this.game.switchTurn();
     }
     return this.game.getWinner();
   }
